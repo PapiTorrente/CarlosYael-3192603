5636bfe120ddc2e486e172c6e51fefd4cbcab5bd
Torrente_ <carlos.tenorio.14.81.33@gmail.com>
2024-08-21 18:15:58 -0600

    Actualización [21-08-24]

---
+++ Tarea_2/app.js
@@ -0,0 +1,10 @@
+let num1 = prompt("Escribe a continuación un número: ");
+let num2 = prompt("Escribe a continuación un número: ");
+
+if (num1 > num2) {
+    console.log("Tu primer número es mayor que el primero");
+} else if (num1 < num2) {
+    console.log("Tu segundo número es mayor que el primero");
+} else if (num1 === num2) {
+    console.log("Tus numeros son exactamente iguales :)");
+}
