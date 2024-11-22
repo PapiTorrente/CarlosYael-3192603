4af791ae354a88cd90a3d94d23d1f740d2556dc4
PapiTorrente <carlos.tenorio.14.81.33@gmail.com>
2024-11-21 23:10:55 -0600

    Carga del proyecto final - TERMINADO

--- Tarea_12/app.js
+++ Tarea_12/app.js
@@ -2,93 +2,34 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 
-
 const app = express();
 
 //Configuracion para el uso peticiones post
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use( bodyParser.urlencoded({extended:false}));
 
-
-//Plantillas dinamicas
-app.set('view engine', 'ejs');
+//Plantillas que sean dinamicas
+app.set('view engine','ejs');
 
 //Crear la conexion
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // tu usuario de MySQL
-    password: '12345678', // tu contraseña de MySQL
-    database: 'baseDatos',
-    port: 3306
+    host: 'localhost',//server
+    user: 'root',//usuario de la DB
+    password: '1234',//pass de tu DB
+    database: 'baseDatos',//nombre de la base de datos
+    port: 3306//puerto
 });
 
-
 //Comprobacion de la conexion de la base de datos
-db.connect(err => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-    } else {
-        console.log('Connected to the MySQL database');
+db.connect(err=>{
+    if(err){
+        console.log(`Error en la conexion de base de datos ${err}`);
+    }else {
+        console.log(`La base de datos funciona y esta conectada.`);    
     }
 });
 
 //Iniciamos el server
 const port = 3306;
-app.listen(port, ()=>{
-    console.log(`Servidor funcionando en http://localhost:${port}`);
-});
-
-//Index
-app.get('/', (req, res) => {
-    const query = 'SELECT * FROM users';
-    db.query(query, (err, results) => {
-        if (err) {
-            console.error('Error fetching users:', err);
-            res.send('Error');
-        } else {
-            res.render('index', { users: results });
-        }
-    });
-});
-
-
-//Agregar usuarios
-app.post('/add', (req, res) => {
-    const { name, email } = req.body;
-    const query = 'INSERT INTO users (name, email) VALUES (?, ?)';
-    db.query(query, [name, email], (err) => {
-        if (err) {
-            console.error('Error adding user:', err);
-            res.send('Error');
-        } else {
-            res.redirect('/');
-        }
-    });
-});
-
-//Editar usuario
-app.get('/edit/:id', (req, res) => {
-    const { id } = req.params;
-    const query = 'SELECT * FROM users WHERE id = ?';
-    db.query(query, [id], (err, results) => {
-        if (err) {
-            console.error('Error fetching user:', err);
-            res.send('Error');
-        } else {
-            res.render('edit', { user: results[0] });
-        }
-    });
-});
-
-//Eliminar usuario
-app.get('/delete/:id', (req, res) => {
-    const { id } = req.params;
-    const query = 'DELETE FROM users WHERE id = ?';
-    db.query(query, [id], (err) => {
-        if (err) {
-            console.error('Error deleting user:', err);
-            res.send('Error');
-        } else {
-            res.redirect('/');
-        }
-    });
+app.listen(port,()=>{
+    console.log(`Servidor en funcionamiento desde http://localhost:${port}`);
 });
---
+++ Tarea_13/aplicacion.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Configuracion para el uso peticiones post
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Configuración para servir archivos estáticos desde la carpeta "public"
+app.use(express.static('public'));
+
+//Plantillas Dinámicas
+app.set('view engine', 'ejs');
+
+//Conexión a la base de datos
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', // tu usuario de MySQL
+    password: '1234', // tu contraseña de MySQL
+    database: 'registros',
+    port: 3306
+});
+
+
+//Comprobacion de la conexión de la base de datos
+db.connect(err => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+    } else {
+        console.log('Connected to the MySQL database');
+    }
+});
+
+//INICIALIZACIÓN DEL SERVER
+const port = 3006;
+app.listen(port,()=>{
+    console.log(`Servidor en funcionamiento desde http://localhost:${port}`);
+});
+
+//Ruta index
+app.get('/', (req, res) => {
+    const query = 'SELECT * FROM usuarios';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching users:', err);
+            res.send('Error');
+        } else {
+            res.render('index', { usuarios: results });
+        }
+    });
+});
+
+//Agregar usuarios
+app.post('/add', (req, res) => {
+    const { nombre, ap_paterno, ap_materno, edad, oficio, correo, telefono, direccion } = req.body;
+    const query = 'INSERT INTO usuarios (nombre, ap_paterno, ap_materno, edad, oficio, correo, telefono, direccion) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    db.query(query, [nombre,ap_paterno, ap_materno, edad, oficio, correo, telefono, direccion], (err) => {
+        if (err) {
+            console.error('Error adding user:', err);
+            res.send('Error');
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+//Editar usuario
+app.get('/edit/:id', (req, res) => {
+    const { id } = req.params;
+    const query = 'SELECT * FROM usuarios WHERE id_usr = ?';
+    db.query(query, [id], (err, results) => {
+        if (err) {
+            console.error('Error fetching user:', err);
+            res.send('Error');
+        } else {
+            res.render('edit', { usuarios: results[0] });
+        }
+    });
+});
+
+//Eliminar usuario
+app.get('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    const query = 'DELETE FROM usuarios WHERE id_usr = ?';
+    db.query(query, [id], (err) => {
+        if (err) {
+            console.error('Error deleting user:', err);
+            res.send('Error');
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+//Actualzar usuario
+app.post('/update/:id', (req, res) => {
+    const { id } = req.params;
+    const { nombre, correo } = req.body;
+    const query = 'UPDATE usuarios SET nombre = ?, ap_paterno = ?, ap_materno = ?, edad = ?, oficio = ?, correo = ?, telefono = ?, direccion = ? WHERE id_usr = ?';
+    db.query(query, [nombre, ap_paterno, ap_materno, edad, oficio, correo, telefono, direccion, id], (err) => {
+        if (err) {
+            console.error('Error adding user:', err);
+            res.send('Error');
+        } else {
+            res.redirect('/');
+        }
+    });
+});
